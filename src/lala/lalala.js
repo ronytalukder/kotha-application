91cce351ce4b1b21a5d254b25ea78056dd9972fb
Rony <you@example.com>
2023-02-10 23:50:18 +0600

    profile picture issu solve

---
+++ src/lala/lalala.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const lalala = () => {
+    return (
+        <div>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora, expedita. Similique voluptas deleniti, deserunt voluptates eligendi aliquam mollitia rem nulla earum libero velit cum provident, repudiandae corrupti commodi ut eius perferendis cupiditate dignissimos et? Molestias obcaecati atque mollitia nesciunt doloribus accusamus eligendi ullam commodi, repellendus alias, ab earum fugit pariatur!</p>
+        </div>
+    );
+};
+
+export default lalala;
