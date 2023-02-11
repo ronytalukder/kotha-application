622680d55b1edc9139b9a68caf686b9f86e48685
Rony <you@example.com>
2023-02-11 23:57:50 +0600

    create user

--- src/components/UserList/UserList.js
+++ src/components/UserList/UserList.js
@@ -1,66 +1,48 @@
 import { Button } from '@material-tailwind/react';
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { FaPlus } from "react-icons/fa";
+import { getDatabase, ref, onValue} from "firebase/database";
+
+
+
+
+
 
 const UserList = () => {
+  const db = getDatabase();
+  const [userList, setUserList] =  useState([])
+
+  useEffect(()=>{
+    const starCountRef = ref(db, 'users');
+    onValue(starCountRef, (snapshot) => {
+      let arr = []
+      snapshot.forEach(item =>{
+        arr.push(item.val())
+      })
+      setUserList(arr)
+    });
+  },[])
+
     return (
         <div>
-            <div className="flex justify-between items-center border-b pb-3 mb-3">
+
+          {
+            userList.map(user => <div className="flex justify-between items-center border-b pb-3 mb-3">
             <div className="flex items-center">
               <div className="w-14 h-14 mr-5 rounded-full overflow-hidden">
                 <img src="images/profile.png" alt="" />
               </div>
               <div>
-                <h3 className="text-base text-black font-bold font-nunito ">Name</h3>
-                <p className="text-sm">10:45:10</p>
+                <h3 className="text-base text-black font-bold font-nunito ">{user.username}</h3>
+                <p className="text-sm">{user.email}</p>
               </div>
             </div>
             <div>
-            <Button className="font-nunito" size="sm" color="green"> <FaPlus className='text-base'></FaPlus> </Button>
+            <Button className="font-nunito" size="sm" color="green"> Add <FaPlus className='text-sm'></FaPlus> </Button>
             </div>
-          </div>
-            <div className="flex justify-between items-center border-b pb-3 mb-3">
-            <div className="flex items-center">
-              <div className="w-14 h-14 mr-5 rounded-full overflow-hidden">
-                <img src="images/profile.png" alt="" />
-              </div>
-              <div>
-                <h3 className="text-base text-black font-bold font-nunito ">Name</h3>
-                <p className="text-sm">10:45:10</p>
-              </div>
-            </div>
-            <div>
-            <Button className="font-nunito" size="sm" color="green"> <FaPlus className='text-base'></FaPlus> </Button>
-            </div>
-          </div>
-            <div className="flex justify-between items-center border-b pb-3 mb-3">
-            <div className="flex items-center">
-              <div className="w-14 h-14 mr-5 rounded-full overflow-hidden">
-                <img src="images/profile.png" alt="" />
-              </div>
-              <div>
-                <h3 className="text-base text-black font-bold font-nunito ">Name</h3>
-                <p className="text-sm">10:45:10</p>
-              </div>
-            </div>
-            <div>
-            <Button className="font-nunito" size="sm" color="green"> <FaPlus className='text-base'></FaPlus> </Button>
-            </div>
-          </div>
-            <div className="flex justify-between items-center border-b pb-3 mb-3">
-            <div className="flex items-center">
-              <div className="w-14 h-14 mr-5 rounded-full overflow-hidden">
-                <img src="images/profile.png" alt="" />
-              </div>
-              <div>
-                <h3 className="text-base text-black font-bold font-nunito ">Name</h3>
-                <p className="text-sm">10:45:10</p>
-              </div>
-            </div>
-            <div>
-            <Button className="font-nunito" size="sm" color="green"> <FaPlus className='text-base'></FaPlus> </Button>
-            </div>
-          </div>
+          </div> )
+          }
+           
         </div>
     );
 };
